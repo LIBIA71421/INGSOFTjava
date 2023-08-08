ca5a124cbeea266d48f0150877af0d4ae137b5e1
Clase <baka65311131@gmail.com>
2023-08-08 10:30:10 -0400

    Anadi el boton Multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,30 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
+const firstM = document.querySelector("#primer-numeroM");
+const secondM = document.querySelector("#segundo-numeroM");
+const formM = document.querySelector("#multiplicar-form");
+const divM = document.querySelector("#resultado-divM");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
-
+  
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+formM.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumberM = Number.parseInt(firstM.value);
+  const secondNumberM = Number.parseInt(secondM.value);
+
+  divM.innerHTML = "<p>" + multiplicar(firstNumberM, secondNumberM) + "</p>";
 });
